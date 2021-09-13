a286431d7dc6dd90bf4bf67343f46a02810368cc
Shaviaditya <rudrashiv311@gmail.com>
2021-09-13 22:43:00 +0530

    Added variable support of integers and characters such that they are approapriately read and printed in C lang

--- tokenizer.cpp
+++ tokenizer.cpp
@@ -11,14 +11,28 @@ int main()
     varKey.insert({"in", "int"});
     varKey.insert({"ch", "char"});
     varKey.insert({"st", "char[]"});
-    varKey.insert({"<htpl>", "#include<stdio.h>\nint main()\n{"});
+    varKey.insert({"<htpl>", "#include<stdio.h>\n int main()\n{"});
     varKey.insert({"/", "}"});
     varKey.insert({"<log>", "printf()"});
     varKey.insert({"/log", ";"});
+    varKey.insert({"int", "%d"});
+    varKey.insert({"char", "%c"});
+    varKey.insert({"string", "%s"});
     std::ifstream readData("input.txt");
+    std::unordered_map<std::string, std::pair<std::string, std::string>> varStore;
     while (getline(readData, res))
     {
-        res.erase(remove(res.begin(), res.end(), ' '), res.end());
+        //res.erase(remove(res.begin(), res.end(), ' '), res.end());
+        int cnt = 0;
+        for (int i = 0; i < res.length(); i++)
+        {
+            if (res[i] != ' ')
+            {
+                break;
+            }
+            cnt++;
+        }
+        res = res.substr(cnt, res.length() - cnt);
         if (res[0] == '<')
         {
             std::unordered_map<std::string, std::string>::iterator it;
@@ -33,6 +47,31 @@ int main()
                 res = "/";
                 it = varKey.find(res);
                 objInsert.push_back(it->second);
+            }
+            else if (res[res.length() - 1] == '>' && res[res.length() - 2] == '/')
+            {
+                res = res.substr(1, res.length() - 3) + " ";
+                std::vector<std::string> tmp;
+                std::string stf = "";
+                for (int i = 0; i < res.length(); i++)
+                {
+                    if (res[i] != ' ')
+                    {
+                        stf = stf + res[i];
+                    }
+                    else
+                    {
+                        if (stf != "=")
+                        {
+                            tmp.push_back(stf);
+                        }
+                        stf = "";
+                    }
+                }
+                it = varKey.find(tmp[0]);
+                res = it->second + res.substr(res.find(' '), res.length() - res.find(' ') - 1) + ';';
+                objInsert.push_back(res);
+                varStore.insert({tmp[1], {tmp[0], tmp[2]}});
             }
             else
             {
@@ -52,20 +91,38 @@ int main()
             }
         }
     }
+    // for(int i=0;i<objInsert.size();i++){
+    //     std::cout<<objInsert[i]<<"\n";
+    // }
+    // for(auto it: varStore){
+    //     std::cout<<it.first<<" "<<(it.second).first<<" "<<it.second.second<<"\n";
+    // }
+    std::unordered_map<std::string, std::pair<std::string, std::string>>::iterator itr;
     for (int i = 0; i < objInsert.size(); i++)
     {
         if (objInsert[i] == "printf()" && objInsert[i + 2] == ";")
         {
-            objInsert[i + 1] = '"' + objInsert[i + 1] + '"';
-            objInsert[i] = objInsert[i].substr(0, 7) + objInsert[i + 1] + objInsert[i].substr(7, 1) + objInsert[i + 2];
-            objInsert.erase(objInsert.begin() + i + 1);
-            objInsert.erase(objInsert.begin() + i + 1);
+            itr = varStore.find(objInsert[i + 1]);
+            if (itr != varStore.end())
+            {
+                objInsert[i] = objInsert[i].substr(0, 7) + '"' + varKey.find((varKey.find(itr->second.first))->second)->second + "%c" + '"' + "," + objInsert[i + 1] + ',' + "10" + objInsert[i].substr(7, 1) + objInsert[i + 2];
+                objInsert.erase(objInsert.begin() + i + 1);
+                objInsert.erase(objInsert.begin() + i + 1);
+            }
+            else
+            {
+                objInsert[i + 1] = '"' + objInsert[i + 1] + "%c" + '"' + ',' + "10";
+                objInsert[i] = objInsert[i].substr(0, 7) + objInsert[i + 1] + objInsert[i].substr(7, 1) + objInsert[i + 2];
+                objInsert.erase(objInsert.begin() + i + 1);
+                objInsert.erase(objInsert.begin() + i + 1);
+            }
         }
     }
     std::ofstream writeIn("output.c");
     for (int i = 0; i < objInsert.size(); i++)
     {
         writeIn << objInsert[i] << "\n";
+        //writeIn << "println('\n')"<< "\n";
     }
     writeIn.close();
     readData.close();
