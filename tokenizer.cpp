9dab04b567ed318d44bf7ebc40dd40ca0554a9bd
Architlall <archit10dgp@gmail.com>
2021-09-15 23:18:47 +0530

    refactored code with user input

--- tokenizer.cpp
+++ tokenizer.cpp
@@ -3,40 +3,177 @@
 #include <algorithm>
 #include <vector>
 #include <unordered_map>
-int main()
+#include "token_dataset.hpp"
+std::unordered_map<std::string, std::pair<std::string, std::string>> varStore;
+std::vector<std::string> codeSnippets;
+//Generates an output C-Lang file 
+void ouputFileReader()
 {
-    std::string res;
-    std::unordered_map<std::string, std::string> varKey;
-    std::vector<std::string> objInsert;
-    varKey.insert({"in", "int"});
-    varKey.insert({"ch", "char"});
-    varKey.insert({"take", "scanf()"});
-    varKey.insert({"st", "char[]"});
-    varKey.insert({"<htpl>", "#include<stdio.h>\n int main()\n{"});
-    varKey.insert({"/", "}"});
-    varKey.insert({"<log>", "printf()"});
-    varKey.insert({"/log", ";"});
-    varKey.insert({"int", "%d"});
-    varKey.insert({"char", "%c"});
-    varKey.insert({"string", "%s"});
-
-    std::ifstream readData("input.txt");
-    std::unordered_map<std::string, std::pair<std::string, std::string>> varStore;
-    while (getline(readData, res))
+    std::ofstream writeIn("output.c");
+    for (int i = 0; i < codeSnippets.size(); i++)
     {
-        //res.erase(remove(res.begin(), res.end(), ' '), res.end());
-        int cnt = 0;
-        for (int i = 0; i < res.length(); i++)
+        writeIn << codeSnippets[i] << "\n";
+    }
+    writeIn.close();
+}
+
+//Removes beginning whitespaces from each line
+std::string removeWhitespace(std::string req){
+    int cnt = 0;
+    for (int i = 0; i < req.length(); i++)
+    {
+        if (req[i] != ' ')
         {
-            if (res[i] != ' ')
-            {
-                break;
-            }
-            cnt++;
+            break;
         }
-        std::cout << res << "\n";
-        res = res.substr(cnt, res.length() - cnt);
-        if (res[0] == '<')
+        cnt++;
+    }
+    req = req.substr(cnt, req.length() - cnt);
+    return req;
+}
+
+//Adjust Print statements in the vector
+void printlnParserStream()
+{
+    dataSet();
+    std::unordered_map<std::string, std::pair<std::string, std::string>>::iterator itr;
+    // for (int i = 0; i < codeSnippets.size(); i++)
+    // {
+    //     //fills the println statment with required result
+    //     if (codeSnippets[i] == "printf()" && codeSnippets[i + 2] == ";")
+    //     {
+    //         itr = varStore.find(codeSnippets[i + 1]);
+    //         //Deconstructs the print statements to be inserted
+    //         if (itr != varStore.end())
+    //         {
+    //             //For variable types like e.g println("%d",var_name);
+    //             codeSnippets[i] = codeSnippets[i].substr(0, 7) + '"' + varKey.find((varKey.find(itr->second.first))->second)->second + "%c" + '"' + "," + codeSnippets[i + 1] + ',' + "10" + codeSnippets[i].substr(7, 1) + codeSnippets[i + 2];
+    //             codeSnippets.erase(codeSnippets.begin() + i + 1);
+    //             codeSnippets.erase(codeSnippets.begin() + i + 1);
+    //         }
+    //         else
+    //         {
+    //             //For variable types like e.g println("_raw_print_");
+    //             codeSnippets[i + 1] = '"' + codeSnippets[i + 1] + "%c" + '"' + ',' + "10";
+    //             codeSnippets[i] = codeSnippets[i].substr(0, 7) + codeSnippets[i + 1] + codeSnippets[i].substr(7, 1) + codeSnippets[i + 2];
+    //             codeSnippets.erase(codeSnippets.begin() + i + 1);
+    //             codeSnippets.erase(codeSnippets.begin() + i + 1);
+    //         }
+    //     }
+    // }
+
+    for (int i = 0; i < codeSnippets.size(); i++)
+        {
+
+            if (codeSnippets[i] == "printf()" && codeSnippets[i + 2] == ";")
+            {
+
+                itr = varStore.find(codeSnippets[i + 1]);
+                if (itr != varStore.end())
+                {
+                    codeSnippets[i] = codeSnippets[i].substr(0, 7) + '"' + varKey.find((varKey.find(itr->second.first))->second)->second + "%c" + '"' + "," + codeSnippets[i + 1] + ',' + "10" + codeSnippets[i].substr(7, 1) + codeSnippets[i + 2];
+                    codeSnippets.erase(codeSnippets.begin() + i + 1);
+                    codeSnippets.erase(codeSnippets.begin() + i + 1);
+                }
+                else
+                {
+                    codeSnippets[i + 1] = '"' + codeSnippets[i + 1] + "%c" + '"' + ',' + "10";
+                    codeSnippets[i] = codeSnippets[i].substr(0, 7) + codeSnippets[i + 1] + codeSnippets[i].substr(7, 1) + codeSnippets[i + 2];
+                    codeSnippets.erase(codeSnippets.begin() + i + 1);
+                    codeSnippets.erase(codeSnippets.begin() + i + 1);
+                }
+            }
+
+            else if (codeSnippets[i] == "scanf()")
+            {
+                std::cout <<"helll"<< "\n";
+                std::cout << codeSnippets[i+1]<< "\n";
+               
+                itr = varStore.find(codeSnippets[i + 1]);
+                 std::cout << varKey.find((varKey.find(itr->second.first))->second)->second<< "\n";
+                // if (itr != varStore.end())
+                {
+                    codeSnippets[i] = codeSnippets[i].substr(0, 6) + '"' + varKey.find((varKey.find(itr->second.first))->second)->second + '"' + "," + '&' + codeSnippets[i + 1]  + ')' +';' ;
+                    std::cout <<codeSnippets[i]<< "\n";
+                     codeSnippets.erase(codeSnippets.begin() + i + 1);
+                   
+                    
+                }
+            }
+        }
+}
+//Main logic resides here as this part deconstructs each line to fill in the codeSnippets
+void fileVectorBuilder(std::string res)
+{
+    dataSet();
+    res = removeWhitespace(res);
+    // if (res[0] == '<')
+    // {
+    //     //Searchs for an item by substring and injects it in vectorStream
+    //     if (res.substr(1, 4) == "/log")
+    //     {
+    //         res = "/log";
+    //         codeSnippets.push_back(varKey.find(res)->second);
+    //     }
+    //     else if (res[1] == '/' && res.substr(2, 3) != "log")
+    //     {
+    //         res = "/";
+    //         codeSnippets.push_back(varKey.find(res)->second);
+    //     }
+    //     else if (res[res.length() - 1] == '>' && res[res.length() - 2] == '/')
+    //     {
+    //         res = res.substr(1, res.length() - 3) + " ";
+    //         std::vector<std::string> tmp;
+    //         std::string stf = "";
+    //         int x = 0;
+    //         for (int i = 0; i < res.length(); i++)
+    //             {
+    //                 if (res[i] == '=')
+    //                 {
+    //                     x = 1;
+    //                     break;
+    //                 }
+    //             }
+
+
+    //         for (int i = 0; i < res.length(); i++)
+    //         {
+    //             if (res[i] != ' ')
+    //             {
+    //                 stf = stf + res[i];
+    //             }
+    //             else
+    //             {
+    //                 if (stf != "=")
+    //                 {
+    //                     tmp.push_back(stf);
+    //                 }
+    //                 stf = "";
+    //             }
+    //         }
+    //         res = varKey.find(tmp[0])->second + res.substr(res.find(' '), res.length() - res.find(' ') - 1) + ';';
+    //         codeSnippets.push_back(res);
+    //         varStore.insert({tmp[1], {tmp[0], tmp[2]}});
+    //     }
+    //     else
+    //     {
+    //         codeSnippets.push_back(varKey.find(res)->second);
+    //     }
+    // }
+    // else
+    // {
+    //     if (codeSnippets[codeSnippets.size() - 1] == "printf()")
+    //     {
+    //         codeSnippets.push_back(res);
+    //     }
+    //     else
+    //     {
+    //         codeSnippets[codeSnippets.size() - 1] += res;
+    //     }
+    // }
+
+
+     if (res[0] == '<')
         {
             std::cout << res << "\n";
             std::unordered_map<std::string, std::string>::iterator it;
@@ -44,14 +181,14 @@ int main()
             {
                 res = "/log";
                 it = varKey.find(res);
-                objInsert.push_back(it->second);
+                codeSnippets.push_back(it->second);
             }
             else if (res[1] == '/' && res.substr(2, 3) != "log")
             {
 
                 res = "/";
                 it = varKey.find(res);
-                objInsert.push_back(it->second);
+                codeSnippets.push_back(it->second);
             }
             else if (res[res.length() - 1] == '>' && res[res.length() - 2] == '/')
             {
@@ -91,7 +228,7 @@ int main()
                     it = varKey.find(tmp[0]);
                     res = it->second + res.substr(res.find(' '), res.length() - res.find(' ') - 1) + ';';
                     std::cout << res << "\n";
-                    objInsert.push_back(res);
+                    codeSnippets.push_back(res);
                     varStore.insert({tmp[1], {tmp[0], tmp[2]}});
                 }
                 else if (x == 0)
@@ -114,17 +251,17 @@ int main()
                     // res = it->second + ' ' + tmp[1] + ';';
                     res = it->second + res.substr(res.find(' '), res.length() - res.find(' ') - 1) + ';';
                     std::cout << res[0] << "\n";
-                    objInsert.push_back(res);
-                    std::cout << objInsert[objInsert.size() - 1] << "\n";
+                    codeSnippets.push_back(res);
+                    std::cout << codeSnippets[codeSnippets.size() - 1] << "\n";
                     varStore.insert({tmp[1], {tmp[0], "NULL"}});
                     }
                     else if(tmp[0] == "take"){
                         it = varKey.find(tmp[0]);
                         res = it->second;
-                        objInsert.push_back(res);
-                        objInsert.push_back(tmp[1]);
-                        std::cout<<objInsert[objInsert.size()-1]<<"\n";
-                        std::cout<<objInsert[objInsert.size()-2]<<"\n";
+                        codeSnippets.push_back(res);
+                        codeSnippets.push_back(tmp[1]);
+                        std::cout<<codeSnippets[codeSnippets.size()-1]<<"\n";
+                        std::cout<<codeSnippets[codeSnippets.size()-2]<<"\n";
                     }
                 }
             }
@@ -132,80 +269,43 @@ int main()
             {
 
                 it = varKey.find(res);
-                objInsert.push_back(it->second);
+                codeSnippets.push_back(it->second);
             }
         }
         else
         {
-            if (objInsert[objInsert.size() - 1] == "printf()")
+            if (codeSnippets[codeSnippets.size() - 1] == "printf()")
             {
                 std::cout << res << "\n";
-                objInsert.push_back(res);
+                codeSnippets.push_back(res);
             }
-            else if (objInsert[objInsert.size() - 1] == "scanf()")
+            else if (codeSnippets[codeSnippets.size() - 1] == "scanf()")
             {
-                objInsert.push_back(res);
+                codeSnippets.push_back(res);
             }
 
             else
             {
-                objInsert[objInsert.size() - 1] += res;
+                codeSnippets[codeSnippets.size() - 1] += res;
             }
         }
-        // for(int i=0;i<objInsert.size();i++){
-        //     std::cout<<objInsert[i]<<"\n";
-        // }
-        // for(auto it: varStore){
-        //     std::cout<<it.first<<" "<<(it.second).first<<" "<<it.second.second<<"\n";
-        // }
-        std::unordered_map<std::string, std::pair<std::string, std::string>>::iterator itr;
-        for (int i = 0; i < objInsert.size(); i++)
-        {
-
-            if (objInsert[i] == "printf()" && objInsert[i + 2] == ";")
-            {
-
-                itr = varStore.find(objInsert[i + 1]);
-                if (itr != varStore.end())
-                {
-                    objInsert[i] = objInsert[i].substr(0, 7) + '"' + varKey.find((varKey.find(itr->second.first))->second)->second + "%c" + '"' + "," + objInsert[i + 1] + ',' + "10" + objInsert[i].substr(7, 1) + objInsert[i + 2];
-                    objInsert.erase(objInsert.begin() + i + 1);
-                    objInsert.erase(objInsert.begin() + i + 1);
-                }
-                else
-                {
-                    objInsert[i + 1] = '"' + objInsert[i + 1] + "%c" + '"' + ',' + "10";
-                    objInsert[i] = objInsert[i].substr(0, 7) + objInsert[i + 1] + objInsert[i].substr(7, 1) + objInsert[i + 2];
-                    objInsert.erase(objInsert.begin() + i + 1);
-                    objInsert.erase(objInsert.begin() + i + 1);
-                }
-            }
-
-            else if (objInsert[i] == "scanf()")
-            {
-                std::cout <<"helll"<< "\n";
-                std::cout << objInsert[i+1]<< "\n";
-               
-                itr = varStore.find(objInsert[i + 1]);
-                 std::cout << varKey.find((varKey.find(itr->second.first))->second)->second<< "\n";
-                // if (itr != varStore.end())
-                {
-                    objInsert[i] = objInsert[i].substr(0, 6) + '"' + varKey.find((varKey.find(itr->second.first))->second)->second + '"' + "," + '&' + objInsert[i + 1]  + ')' +';' ;
-                    std::cout <<objInsert[i]<< "\n";
-                     objInsert.erase(objInsert.begin() + i + 1);
-                   
-                    
-                }
-            }
-        }
-    }
-    std::ofstream writeIn("output.c");
-    for (int i = 0; i < objInsert.size(); i++)
+}
+int main()
+{
+    std::string res;
+    std::ifstream readData("input.txt");
+    while (getline(readData, res))
     {
-        writeIn << objInsert[i] << "\n";
-        //writeIn << "println('\n')"<< "\n";
+        fileVectorBuilder(res);
     }
-    writeIn.close();
+    printlnParserStream();
+    ouputFileReader();
     readData.close();
     return 0;
 }
+
+
+
+
+
+
