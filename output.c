046ab1944d32380f6e9930d7b258157aca3f6dfe
Shaviaditya <rudrashiv311@gmail.com>
2021-09-16 20:52:47 +0530

    Removed commented Codes

---
+++ output.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+ int main()
+{
+int val;
+char rc = 'a';
+scanf("%d",&val);
+printf("%d%c",val,10);
+printf("%c%c",rc,10);
+}
--- tokenizer.cpp
+++ tokenizer.cpp
@@ -37,31 +37,6 @@ void printlnParserStream()
 {
     dataSet();
     std::unordered_map<std::string, std::pair<std::string, std::string>>::iterator itr;
-    // for (int i = 0; i < codeSnippets.size(); i++)
-    // {
-    //     //fills the println statment with required result
-    //     if (codeSnippets[i] == "printf()" && codeSnippets[i + 2] == ";")
-    //     {
-    //         itr = varStore.find(codeSnippets[i + 1]);
-    //         //Deconstructs the print statements to be inserted
-    //         if (itr != varStore.end())
-    //         {
-    //             //For variable types like e.g println("%d",var_name);
-    //             codeSnippets[i] = codeSnippets[i].substr(0, 7) + '"' + varKey.find((varKey.find(itr->second.first))->second)->second + "%c" + '"' + "," + codeSnippets[i + 1] + ',' + "10" + codeSnippets[i].substr(7, 1) + codeSnippets[i + 2];
-    //             codeSnippets.erase(codeSnippets.begin() + i + 1);
-    //             codeSnippets.erase(codeSnippets.begin() + i + 1);
-    //         }
-    //         else
-    //         {
-    //             //For variable types like e.g println("_raw_print_");
-    //             codeSnippets[i + 1] = '"' + codeSnippets[i + 1] + "%c" + '"' + ',' + "10";
-    //             codeSnippets[i] = codeSnippets[i].substr(0, 7) + codeSnippets[i + 1] + codeSnippets[i].substr(7, 1) + codeSnippets[i + 2];
-    //             codeSnippets.erase(codeSnippets.begin() + i + 1);
-    //             codeSnippets.erase(codeSnippets.begin() + i + 1);
-    //         }
-    //     }
-    // }
-
     for (int i = 0; i < codeSnippets.size(); i++)
         {
 
@@ -107,72 +82,6 @@ void fileVectorBuilder(std::string res)
 {
     dataSet();
     res = removeWhitespace(res);
-    // if (res[0] == '<')
-    // {
-    //     //Searchs for an item by substring and injects it in vectorStream
-    //     if (res.substr(1, 4) == "/log")
-    //     {
-    //         res = "/log";
-    //         codeSnippets.push_back(varKey.find(res)->second);
-    //     }
-    //     else if (res[1] == '/' && res.substr(2, 3) != "log")
-    //     {
-    //         res = "/";
-    //         codeSnippets.push_back(varKey.find(res)->second);
-    //     }
-    //     else if (res[res.length() - 1] == '>' && res[res.length() - 2] == '/')
-    //     {
-    //         res = res.substr(1, res.length() - 3) + " ";
-    //         std::vector<std::string> tmp;
-    //         std::string stf = "";
-    //         int x = 0;
-    //         for (int i = 0; i < res.length(); i++)
-    //             {
-    //                 if (res[i] == '=')
-    //                 {
-    //                     x = 1;
-    //                     break;
-    //                 }
-    //             }
-
-
-    //         for (int i = 0; i < res.length(); i++)
-    //         {
-    //             if (res[i] != ' ')
-    //             {
-    //                 stf = stf + res[i];
-    //             }
-    //             else
-    //             {
-    //                 if (stf != "=")
-    //                 {
-    //                     tmp.push_back(stf);
-    //                 }
-    //                 stf = "";
-    //             }
-    //         }
-    //         res = varKey.find(tmp[0])->second + res.substr(res.find(' '), res.length() - res.find(' ') - 1) + ';';
-    //         codeSnippets.push_back(res);
-    //         varStore.insert({tmp[1], {tmp[0], tmp[2]}});
-    //     }
-    //     else
-    //     {
-    //         codeSnippets.push_back(varKey.find(res)->second);
-    //     }
-    // }
-    // else
-    // {
-    //     if (codeSnippets[codeSnippets.size() - 1] == "printf()")
-    //     {
-    //         codeSnippets.push_back(res);
-    //     }
-    //     else
-    //     {
-    //         codeSnippets[codeSnippets.size() - 1] += res;
-    //     }
-    // }
-
-
      if (res[0] == '<')
         {
             std::cout << res << "\n";
